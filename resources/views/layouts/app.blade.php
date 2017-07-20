d23768a8861041818dc52e5b4edac09516d211fa
aliaimviz <ali.aimviz@gmail.com>
2017-07-20 17:13:18 +0500

    app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -159,23 +159,7 @@
             <!-- </div> -->
         </footer>
     </div>
-<<<<<<< HEAD
-    <!-- scripts -->
-<script src="{{ asset('public/js/jquery-3.2.1.min.js') }}"></script>  
-   <script src="{{ asset('public/js/bootstrap.min.js') }}"></script> 
-   <script src="<?php echo e(asset('public/js/config.js')); ?>"></script>
-   <script src="<?php echo e(asset('public/js/util.js')); ?>"></script>
-   <script src="<?php echo e(asset('public/js/jquery.emojiarea.js')); ?>"></script>
-   <script src="<?php echo e(asset('public/js/emoji-picker.js')); ?>"></script>
-   <script src="<?php echo e(asset('public/js/tether.min.js')); ?>"></script>    
-   <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
-   <script src="<?php echo e(asset('public/js/app.js')); ?>"></script>
-   <!-- Bootstrap tags input -->
 
-    <script src="{{ asset('public/js/bootstrap-tagsinput.js') }}"></script>
-   <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>    
-   <script src="{{ asset('public/js/bootstrap-tagsinput-angular.js') }}"></script>     
-=======
 
     
     <script src="{{ asset('public/js/jquery-3.2.1.min.js') }}"></script>  
@@ -192,7 +176,7 @@
     <script src="{{ asset('public/js/bootstrap-tagsinput.js') }}"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js"></script>    
     <script src="{{ asset('public/js/bootstrap-tagsinput-angular.js') }}"></script>    
->>>>>>> 03d9e46402441f806065ea574e04fa372b88f416
+
 
 </body>
 </html>
